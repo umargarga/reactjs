522721a4b5f6d9667f389f352a904ae4d7958d89
Umar Yusuf Dayyabu <udayyabuyusuf@edu.salford.ac.uk>
2021-03-09 11:45:32 +0100

    Added the delete function

--- src/App.js
+++ src/App.js
@@ -23,10 +23,14 @@ const App = () => {
         reminder: false,
     }
 ])
+
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks to Show'}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,9 @@
-const Task = ({ task }) => {
+import { FaTimes } from 'react-icons/fa'
+const Task = ({ task, onDelete }) => {
     return (
         <div className='card mb-1'>
             <div className="card-header">
-                {task.text }
+                {task.text } <FaTimes className='text-danger float-right' onClick={() => onDelete(task.id)} />
             </div>
             <div className='card-body'>
                 <p>{task.day}</p>
